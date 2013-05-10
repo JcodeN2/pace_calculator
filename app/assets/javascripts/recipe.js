62b493f7702d2985390154c2df0cb41a3573ac47
Jennifer Ngetich <jennifer.ngetich@gmail.com>
2013-05-10 13:29:14 -0400

    Initial Commit

---
+++ app/assets/javascripts/recipe.js
@@ -0,0 +1,24 @@
+Parse.initialize("5o4zSys3PXx1bRxApmLnlGJhzPJ8TegVes0bfeCP", "8MXhQEhJes123dssbJo3dj7bqfx0WusZ1pdQGlk0");
+
+$(document).ready ->
+ 
+ 
+  paces = [{
+      idnum : 1        
+      fivek : "19:00"            
+      tenk : "40:00"             
+      harmaranth : "01:30"                
+      marathon : "03:45"                
+    },
+    {
+      idnum : 2        
+      fivek : "20:00"            
+      tenk : "42:00"             
+      harmaranth : "01:35"                
+      marathon : "04:00"               
+    }
+  ]
+  
+  $.each paces, (index, pace) ->
+    template = JST["templates/test-template"] pace
+    $('.test-div').append template
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+pace_calculator::Application.routes.draw do
+  get 'test' => 'pages#test'
+  get 'recipe_preview' => 'pages#recipe_preview'
+  get 'newrecipe' => 'pages#newrecipe'
+  get 'dropdown_menu' => 'pages#dropdown_menu'
+end
